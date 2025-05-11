de16588789198e8472d3dbdabda4106360ce9fe8
RazanQaddomi <qaddomirazan14@gmail.com>
2025-05-11 08:47:54 +0300

    Add JobDetailsJopSeekerScreen to display job details with AppBar and close button

---
+++ ojps_flutter/lib/screens/job_details_job_seeker_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:ojps_flutter/constants/colors.dart';
+import 'package:ojps_flutter/widgets/job_details_job_seeker_widget.dart';
+
+class JobDetailsJopSeekerScreen extends StatelessWidget {
+  const JobDetailsJopSeekerScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: whiteColor,
+      appBar: AppBar(
+        backgroundColor: whiteColor,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.close, color: primaryTextColor),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: const JobDetailsJopSeekerWidget(),
+    );
+  }
+}
+
