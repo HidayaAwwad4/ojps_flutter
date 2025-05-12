ac86e6fa70bbc772f277e699a80cc4c8f40f633d
RazanQaddomi <151151234+RazanQaddomi@users.noreply.github.com>
2025-05-13 01:41:33 +0300

    update main with splash

--- ojps_flutter/lib/main.dart
+++ ojps_flutter/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:ojps_flutter/constants/colors.dart';
+import 'package:ojps_flutter/screens/splash_screen.dart';
+import 'package:ojps_flutter/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +13,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'OJPS',
       debugShowCheckedModeBanner: false,
-      //home: ,
+      theme: ThemeData(
+        primaryColor: primaryColor,
+        scaffoldBackgroundColor: whiteColor,
+        appBarTheme: AppBarTheme(
+          backgroundColor: primaryColor,
+          foregroundColor: whiteColor,
+        ),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: primaryColor,
+          secondary: customLightPurple,
+          background: lightBlueBackgroundColor,
+          surface: cardBackgroundColor,
+          error: closedColor,
+          onPrimary: whiteColor,
+          onSecondary: secondaryTextColor,
+        ),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: primaryTextColor),
+          bodyMedium: TextStyle(color: secondaryTextColor),
+        ),
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/home': (context) => const HomeScreen(),
+      },
     );
   }
 }
