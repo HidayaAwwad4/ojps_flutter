1bf3578e1317a82360392f6e4855a2d922d9e997
haneenakobeh <s12114178@stu.najah.edu>
2025-05-16 22:31:31 +0300

    view profile tile widget

--- ojps_flutter/lib/constants/dimensions.dart
+++ ojps_flutter/lib/constants/dimensions.dart
@@ -1,1 +1,6 @@
 const double defaultPadding = 16.0;
+const double defaultRadius = 12.0;
+const double borderRadius = 12.0;
+const double profileImageRadius = 50.0;
+const double iconSize = 24.0;
+const double fontSizeNormal = 16.0;
