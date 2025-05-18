557fb32019821d0e6c0e6fb9eef4dbf4f3415894
haneenakobeh <s12114178@stu.najah.edu>
2025-05-18 23:16:24 +0300

    notification widget

---
+++ ojps_flutter/lib/constants/text_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+const textStyleWhite = TextStyle(color: Colors.white, fontSize: 12);
+const textStyleWhiteBold = TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 14);
+
+const textStylePrimaryBold = TextStyle(color: primaryColor, fontWeight: FontWeight.bold, fontSize: 14);
+const textStyleSmallGrey = TextStyle(color: Colors.grey, fontSize: 12);
+
+const textStyleHeader = TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
+const textStyleAppBar = TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
