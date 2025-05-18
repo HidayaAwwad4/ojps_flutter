260c3bb00843b7733b082ca30e8b335c52d4dea8
Hidaya Awwad <hidayaibrahimawwad4@gmail.com>
2025-05-18 08:54:28 +0300

    Remove desktop.ini and ignore it in future

--- ojps_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ojps_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import file_picker
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
 import open_file_mac
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
 }
