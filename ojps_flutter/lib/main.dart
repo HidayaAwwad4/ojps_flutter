0d14db63092a4da63bbbebf79e9458978afca741
RazanQaddomi <151151234+RazanQaddomi@users.noreply.github.com>
2025-05-22 20:12:51 +0300

    update main

---
+++ ojps_flutter/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:ojps_flutter/constants/colors.dart';
+import 'package:ojps_flutter/constants/routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'OJPS',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: primaryColor,
+        scaffoldBackgroundColor: whiteColor,
+        appBarTheme: AppBarTheme(
+          backgroundColor: primaryColor,
+          foregroundColor: whiteColor,
+        ),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: primaryColor,
+          secondary: customLightPurple,
+          background: lightBlueBackgroundColor,
+          surface: cardBackgroundColor,
+          error: closedColor,
+          onPrimary: whiteColor,
+          onSecondary: secondaryTextColor,
+        ),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: primaryTextColor),
+          bodyMedium: TextStyle(color: secondaryTextColor),
+        ),
+      ),
+      initialRoute: '/',
+      routes: appRoutes, // استخدم هنا الخريطة من ملف routes.dart
+    );
+  }
+}
