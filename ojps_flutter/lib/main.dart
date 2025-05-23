8c58c31478934637ac78ed00c992e4ca8a00fb09
tasneemjber <tasneemjber@gmail.com>
2025-05-23 18:38:34 +0300

    حفظ التعديلات قبل التحويل لفرع development

--- ojps_flutter/lib/main.dart
+++ ojps_flutter/lib/main.dart
@@ -4,6 +4,9 @@ import 'package:ojps_flutter/screens/apply_now_screen.dart';
 import 'package:ojps_flutter/screens/home_screen.dart';
 import 'package:ojps_flutter/screens/job_details_job_seeker_screen.dart';
 import 'package:ojps_flutter/screens/main_screen.dart';
+import 'package:ojps_flutter/screens/Login_page.dart';
+import 'package:ojps_flutter/screens/Forgetpassword.dart';
+import 'package:ojps_flutter/screens/user_type.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +19,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-     // home: ,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/forgetPassword': (context) => ForgetPasswordPage(),
+        '/chooseType': (context) => ChooseType(),
+      },
 
     );
   }
--- ojps_flutter/lib/screens/Login_page.dart
+++ ojps_flutter/lib/screens/Login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ojps_flutter/constants/colors.dart';
 import 'user_type.dart';
 import 'Forgetpassword.dart';
 
@@ -9,7 +10,6 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final Color primaryColor = Color(0xFF0273B1);
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
@@ -21,7 +21,6 @@ class _LoginPageState extends State<LoginPage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-
             Stack(
               children: [
                 ClipPath(
@@ -37,9 +36,6 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ),
                 ),
-
-
-
                 Positioned(
                   top: 20,
                   left: 0,
@@ -94,7 +90,6 @@ class _LoginPageState extends State<LoginPage> {
                       },
                     ),
 
-
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -116,10 +111,7 @@ class _LoginPageState extends State<LoginPage> {
                           alignment: Alignment.centerRight,
                           child: TextButton(
                             onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => ForgetPasswordPage()),
-                              );
+                              Navigator.pushNamed(context, '/forgetPassword');
                             },
                             child: Text(
                               'Forget Password?',
@@ -132,7 +124,6 @@ class _LoginPageState extends State<LoginPage> {
                         ),
                       ],
                     ),
-
 
                     SizedBox(
                       width: double.infinity,
@@ -180,10 +171,7 @@ class _LoginPageState extends State<LoginPage> {
                         Text("Don't have an account?"),
                         TextButton(
                           onPressed: () {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(builder: (context) => ChooseType()),
-                            );
+                            Navigator.pushReplacementNamed(context, '/chooseType');
                           },
                           child: Text(
                             "Sign Up",
@@ -265,6 +253,7 @@ class WaveClipper extends CustomClipper<Path> {
     path.close();
     return path;
   }
+
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
