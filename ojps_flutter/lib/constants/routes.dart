a2dcadff09ee0bf6d7521ee6ad9e09730e18954d
haneenakobeh <s12114178@stu.najah.edu>
2025-06-05 15:14:37 +0300

    add my routs

--- ojps_flutter/lib/constants/routes.dart
+++ ojps_flutter/lib/constants/routes.dart
@@ -6,6 +6,13 @@ import 'package:ojps_flutter/screens/saved_jobs_screen.dart';
 import 'package:ojps_flutter/screens/job_details_job_seeker_screen.dart';
 import 'package:ojps_flutter/screens/job_list_screen.dart';
 
+import '../screens/manage_resume.dart';
+import '../screens/notifications.dart';
+import '../screens/view&edit_profile_seeker.dart';
+import '../screens/view_edit_employer_profile.dart';
+import '../screens/view_profile.dart';
+import '../screens/view_resume.dart';
+
 final Map<String, WidgetBuilder> appRoutes = {
   '/': (context) => const SplashScreen(),
   '/home': (context) => const HomeScreen(),
@@ -13,4 +20,10 @@ final Map<String, WidgetBuilder> appRoutes = {
   '/job_details': (context) => const JobDetailsJobSeekerScreen(),
   '/job_status': (context) => const JobStatusScreen(),
   '/saved_jobs': (context) => const SavedJobsScreen(),
+  '/view_profile_employer':(context)=> const ViewEditEmployerProfile(),
+  '/view_profile_seeker':(context)=> const ViewEditSeekerProfile(),
+  '/view_seeker_profile_employer':(context)=> const  ViewProfile(),
+  '/view_resume':(context)=> const ViewResumeScreen(),
+  '/manage_resume':(context)=> const ManageResumeScreen(),
+  '/notifications':(context)=> const Notifications(),
 };
