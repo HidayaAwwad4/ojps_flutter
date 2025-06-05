c5108a495fc5ddc22a6dd9e1c893778237940910
haneenakobeh <s12114178@stu.najah.edu>
2025-06-05 14:43:27 +0300

    edit constants

--- ojps_flutter/lib/constants/colors.dart
+++ ojps_flutter/lib/constants/colors.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 
-const Color primaryColor = Color(0xFF0273B1);
-const Color cardBackgroundColor = Color(0xFFF0F0F0);
-const Color backgroundColor = Color(0xFFF0F0F0);
-const Color lightBlueBackgroundColor = Color(0xFFDDEAF3);
-const Color customLightPurple = Color(0xFFE1BEE7);
-const Color closedColor = Colors.red;
-const Color openColor = Colors.green;
-const Color pendingColor = Colors.orange;
-const Color primaryTextColor = Colors.black;
-const Color secondaryTextColor = Colors.black87;
-const Color whiteColor = Colors.white;
-const Color profileColor = Color(0xFFE8EEF1);
-const Color lightGrey = Color(0xFFA0A4A5);
-const Color successColor = Color(0xFF28A745);
-const Color greyColor = Colors.grey;
+class Colorss {
+  static const Color primaryColor = Color(0xFF0273B1);
+  static const Color cardBackgroundColor = Color(0xFFF0F0F0);
+  static const Color backgroundColor = Color(0xFFF0F0F0);
+  static const Color lightBlueBackgroundColor = Color(0xFFDDEAF3);
+  static const Color customLightPurple = Color(0xFFE1BEE7);
+  static const Color closedColor = Colors.red;
+  static const Color openColor = Colors.green;
+  static const Color pendingColor = Colors.orange;
+  static const Color primaryTextColor = Colors.black;
+  static const Color secondaryTextColor = Colors.black87;
+  static const Color whiteColor = Colors.white;
+  static const Color blackColor = Colors.black;
+  static const Color profileColor = Color(0xFFE8EEF1);
+  static const Color lightGrey = Color(0xFFA0A4A5);
+  static const Color successColor = Color(0xFF28A745);
+  static const Color greyColor = Colors.grey;
+  static const Color buttonInactiveBackgroundColor = Color(0xFFE8E8E8);
+  static const Color buttonInactiveTextColor = Color(0xFFADADAD);
+  static const Color successValidation = Colors.green;
+  static const Color failValidation = Colors.red;
+}
