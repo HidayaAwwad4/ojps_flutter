7e194f78f6630b855448ed18ae062c064f72aeee
RazanQaddomi <151151234+RazanQaddomi@users.noreply.github.com>
2025-06-09 19:13:32 +0300

    resolve conflict after merge

--- ojps_flutter/lib/services/auth_service.dart
+++ ojps_flutter/lib/services/auth_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class AuthService {
-  final String apiUrl = 'http://127.0.0.1:8000/api';
+  final String apiUrl = 'http://10.0.2.2:8000/api';
 
 
   Future<http.Response> getRoles() async {
